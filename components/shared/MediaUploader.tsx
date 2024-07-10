ebb48af4deacbc09f16ed83e1836eb2fd837a552
Vedant Lokhande <vedantlokhande0101@gmail.com>
2024-07-10 15:55:51 +0530

    commit

--- components/shared/MediaUploader.tsx
+++ components/shared/MediaUploader.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import { useToast } from "@/components/ui/use-toast"
 import { dataUrl, getImageSize } from "@/lib/utils";
 import { CldImage, CldUploadWidget } from "next-cloudinary"
