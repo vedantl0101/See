4cbbd4dd5cc3503f8c264ae5e2d2a16e1aaf53a4
Vedant Lokhande <vedantlokhande0101@gmail.com>
2024-07-10 15:47:29 +0530

    commit

--- lib/actions/image.actions.ts
+++ lib/actions/image.actions.ts
@@ -108,7 +108,7 @@ export async function getAllImages({ limit = 9, page = 1, searchQuery = '' }: {
       secure: true,
     })
 
-    let expression = 'folder=SeeTransform';
+    let expression = 'folder=imaginify';
 
     if (searchQuery) {
       expression += ` AND ${searchQuery}`
