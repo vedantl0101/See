2ff2ec7c20d1bf4a750be061f913226691073248
vedantl0101 <114289860+vedantl0101@users.noreply.github.com>
2024-07-11 20:49:46 +0530

    Add files via upload
    final-- commit

--- middleware.tsx
+++ middleware.tsx
@@ -1,8 +1,9 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
-
-
-export default clerkMiddleware({});
-
+import { authMiddleware } from "@clerk/nextjs/server";
+ 
+export default authMiddleware({
+  publicRoutes: ['/', '/api/webhooks/clerk', '/api/webhooks/stripe']
+});
+ 
 export const config = {
-  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
